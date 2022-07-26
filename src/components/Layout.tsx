9ff1ca1eb2061727489fb0ba9194c04a022eea63
Julius Oh <juliusoh@gmail.com>
2022-07-26 11:43:35 -0700

    ml

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout(props: PropsWithChildren) {
   return (
     <>
       <Navbar />
-      <div className="pl-[24px] pr-[24px] max-w-[1200px] w-[100%] ml-[auto]">
+      <div className="pl-[24px] pr-[24px] max-w-[1200px] w-[100%] ml-[20%]">
         <div className="flex min-h-[100vh]">{props.children}</div>
       </div>
     </>
